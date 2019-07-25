55973fc3d17d9d9ffb95837d852c1d8db154d54f
Enzo D'Onofrio <aethiss@gmail.com>
2019-07-26 00:37:06 +0100

    FEATURE : improved style on search - add new search movie logic and unit test

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import SearchBox from '../searchbox/SearchBox'
 
 import style from "./Header.css"
 
-const Header = () => (
+const Header = (props) => (
   <header className="site-header" role="navigation">
+    <div className="header-middle">
+      <SearchBox {...props} />
+    </div>
     <div className="header-right">
       <img className="header-logo" src="../../../../assets/images/dazn-logo.png" />
     </div>
--- src/components/container/home/Home.js
+++ src/components/container/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import Movies from "../../presentational/movies/Movies"
 import { searchMovie, generateRequestUrl } from '../../../coreAPI/themoviedbAPI'
@@ -6,6 +7,10 @@ import { searchMovie, generateRequestUrl } from '../../../coreAPI/themoviedbAPI'
 import style from "./Home.css"
 
 export default class Home extends Component {
+  static propTypes = {
+    stringSearch: PropTypes.string,
+    searchResult: PropTypes.array,
+  }
 
   constructor(props) {
     super(props)
@@ -16,19 +21,25 @@ export default class Home extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      isLoading: true
-    })
-    searchMovie(generateRequestUrl()).then((res) => {
+    if (!this.props.searchResult.length) {
+      // no search, we looking for a trending movies
       this.setState({
-        isLoading: false,
-        movies: res.results
+        isLoading: true
       })
-    })
+      searchMovie(generateRequestUrl()).then((res) => {
+        this.setState({
+          isLoading: false,
+          movies: res.results
+        })
+      })
+    }
   }
 
   render() {
+    const { searchResult } = this.props
     const { isLoading, movies } = this.state
+
+    const movieList = searchResult.length ? searchResult : movies
 
     return (
       <div className="HomeContainer">
@@ -37,13 +48,14 @@ export default class Home extends Component {
             Loading ...
           </div> :
           <section className="movies">
-            { movies.map((movie) => {
-              return (
+
+            { movieList.map((movie) => {
+              return(
                 <div key={movie.id} className="movie-card">
                   <Movies {...movie} />
                 </div>
               )
-              })
+            })
             }
 
           </section>
